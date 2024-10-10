7205854e77c958c46fba906667bda0f44f892d7f
François VAILLANT <frvaillant@gmail.com>
2024-10-10 21:03:59 +0200

    router in abstract controller

--- Controller/AbstractController.php
+++ Controller/AbstractController.php
@@ -26,9 +26,11 @@ abstract class AbstractController
     {
         global $twig;
         $this->twig = $twig;
+        $this->response = new Response();
+
     }
 
-    protected function publish(string $content)
+    protected function publish(string $content): Response
     {
         $this->response->setContent($content);
         $this->response->setStatusCode(200);
@@ -37,6 +39,8 @@ abstract class AbstractController
 
     protected function redirectTo(string $path = '/'): Response
     {
+        global $_ROUTER;
+        $path = $_ROUTER->getRoutesPaths()[$path] ?? $path;
         $response = new RedirectResponse($path);
         return $response->send();
     }
