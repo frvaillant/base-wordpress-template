c7bec3516556d1d8e002f4bf8e5e5cf1595bf83a
François VAILLANT <frvaillant@gmail.com>
2024-10-19 10:09:41 +0200

    Use method test

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ use App\Router\Router;
 $_ROUTER = new Router();
 add_action( 'init', function () {
     global $_ROUTER;
-    if($_ROUTER->match()) {
+    if($_ROUTER->match() && $_ROUTER->methodIsAllowed()) {
         $_ROUTER->execute();
         die;
     }
