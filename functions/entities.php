7a7ca383aeced3f379f6167ed6f1e82b50e41db0
François VAILLANT <frvaillant@gmail.com>
2024-10-21 08:40:14 +0200

    Remove array() and add []

--- functions/entities.php
+++ functions/entities.php
@@ -10,7 +10,7 @@
 function create_custom_post_type($entityName, $singular, $plurial, $supports = ['title', 'author', 'revisions', 'custom-fields', 'tags', 'page-attributes'])
 {
     // On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
-    $labels = array(
+    $labels = [
         // Le nom au pluriel
         'name'                => _x( $plurial, 'Post Type General Name'),
         // Le nom au singulier
@@ -20,24 +20,24 @@ function create_custom_post_type($entityName, $singular, $plurial, $supports = [
         // Les différents libellés de l'administration
         'all_items'           => __( 'Les ' . $plurial),
         'view_item'           => __( 'Voir les ' .$plurial),
-        'add_new_item'        => __( 'Ajouter ' . $singular),
+        'add_new_item'        => __( 'Ajouter 1 ' . $singular),
         'add_new'             => __( 'Ajouter'),
         'edit_item'           => __( 'Editer ' . $singular),
         'update_item'         => __( 'Modifier ' . $singular),
         'search_items'        => __( 'Rechercher ' . $singular),
         'not_found'           => __( 'Non trouvé'),
         'not_found_in_trash'  => __( 'Non trouvé dans la corbeille'),
-    );
+    ];
 
     // On peut définir ici d'autres options pour notre custom post type
 
-    $args = array(
+    $args = [
         'label'               => __($plurial),
         'description'         => __( 'Tous sur les ' . $plurial),
         'labels'              => $labels,
         // On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
         'supports'            => $supports,
-        'taxonomies'          => array($entityName,  'post_tag' ),
+        'taxonomies'          => [$entityName,  'post_tag'],
         /*
         * Différentes options supplémentaires
         */
@@ -45,16 +45,20 @@ function create_custom_post_type($entityName, $singular, $plurial, $supports = [
         'hierarchical'        => false,
         'public'              => true,
         'has_archive'         => true,
-        'rewrite'			  => array( 'slug' => $entityName),
+        'rewrite'			  => ['slug' => $entityName],
 
-    );
+    ];
 
     register_post_type( $entityName, $args );
 }
 
-
-function entities() {
+/**
+ * @return void
+ */
+function entities(): void
+{
     $entityCollector = new \App\Collectors\EntitiesCollector();
+
     /** @var \App\Annotations\Entity $entity */
     foreach ($entityCollector->getEntities() as $entity) {
         create_custom_post_type($entity->getName(), $entity->getSingular(), $entity->getPlural());
