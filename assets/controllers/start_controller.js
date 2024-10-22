ed4f6aa388f9569a0efc8dc8fcb081729b271050
François VAILLANT <frvaillant@gmail.com>
2024-10-22 18:28:32 +0200

    Doc

--- assets/controllers/start_controller.js
+++ assets/controllers/start_controller.js
@@ -1,8 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
 
+/**
+ * In your html element add data-controller="start".
+ * Its html content will automatically be replaced by "Hello"
+ * Example : <span data-controller="start">Good bye</span>
+ */
 export default class extends Controller {
 
     connect() {
-        this.elemennt.innerHTML = 'hello'
+        this.element.innerHTML = 'hello'
     }
 }
