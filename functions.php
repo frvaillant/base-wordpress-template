94d1ba246b5ce7112bcd1bfc72b488f474450083
François VAILLANT <frvaillant@gmail.com>
2024-10-22 18:02:34 +0200

    move file

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 require('vendor/autoload.php');
 
-include(__DIR__ . '/routes.php');
+include(__DIR__ . '/functions/routes.php');
 include(__DIR__ . '/functions/templates.php');
 include(__DIR__ . '/functions/twig.php');
 include(__DIR__ . '/functions/entities.php');
