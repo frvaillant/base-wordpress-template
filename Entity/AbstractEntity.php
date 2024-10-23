55be5368c6ae6b3e8ca76af44daa52fd5b2cb225
François VAILLANT <frvaillant@gmail.com>
2024-10-23 18:16:42 +0200

    Fix abstract entity

--- Entity/AbstractEntity.php
+++ Entity/AbstractEntity.php
@@ -30,24 +30,26 @@ abstract class AbstractEntity
     {
 
         $post   = get_post($id);
-        $this->hydrateEntity($post);
+        if($post) {
+            $this->hydrateEntity($post);
 
-        $this->tags = get_the_tags($post->ID);
+            $this->tags = get_the_tags($post->ID);
 
-        if (function_exists('acf_is_plugin_active') && acf_is_plugin_active()) {
-            $fields = get_fields($post->ID);
-            if($fields) {
-                $this->hydrateEntity($fields);
+            if (function_exists('acf_is_plugin_active') && acf_is_plugin_active()) {
+                $fields = get_fields($post->ID);
+                if ($fields) {
+                    $this->hydrateEntity($fields);
+                }
             }
+
+            $this->title = $this->post_title;
+
+            $this->content = $this->post_content;
+
+            $this->post_author = get_user_by('ID', $this->post_author);
+
+            $this->author = $this->post_author;
         }
-
-        $this->title = $this->post_title;
-
-        $this->content = $this->post_content;
-
-        $this->post_author = get_user_by('ID', $this->post_author);
-
-        $this->author = $this->post_author;
 
     }
 
