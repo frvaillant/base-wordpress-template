fef073a62e8fee95fa9c69630497011d99e6c525
François VAILLANT <frvaillant@gmail.com>
2024-10-25 11:24:11 +0200

    Change dynamic properties because depreciation

--- Entity/AbstractEntity.php
+++ Entity/AbstractEntity.php
@@ -26,19 +26,27 @@ abstract class AbstractEntity
      */
     public $author;
 
+    /**
+     * @var array
+     */
     public $fields = [];
 
+    /**
+     * @param string|int $id
+     */
     public function __construct(string | int $id)
     {
 
-        $post   = get_post($id);
+        $post   = get_post($id, ARRAY_A);
+
         if($post) {
+            $postId = $post['ID'];
             $this->hydrateEntity($post);
 
-            $this->tags = get_the_tags($post->ID);
+            $this->tags = get_the_tags($postId);
 
             if (function_exists('acf_is_plugin_active') && acf_is_plugin_active()) {
-                $fields = get_fields($post->ID);
+                $fields = get_fields($postId);
                 if ($fields) {
                     $this->hydrateEntity($fields);
                 }
@@ -55,14 +63,23 @@ abstract class AbstractEntity
 
     }
 
-    private function hydrateEntity($array): void
+
+    /**
+     * @param $array
+     * @return void
+     */
+    private function hydrateEntity(array $array): void
     {
         foreach ($array as $key => $value) {
             $this->fields[$key] = $value;
         }
     }
 
-    public function __get(string $name)
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function __get(string $name): mixed
     {
         return $this->fields[$name] ?? null;
     }
